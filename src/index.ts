2a282bf4ec46d63ae9909a2964f3a2cc5d92caf1
Ryan Hirsch <ryan.hirsch@gmail.com>
2019-02-16 14:13:02 -0600

    pointless

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { tokenize, titleCaseWord, untokenize, normalize, isSpecialWord } from "./utils";
 
 export function titleCase(title: string): string {
-  const normalizedTokens = tokenize(title).map(normalize);
-  const titleCasedTokens = normalizedTokens.map((normalizedWord, idx) => {
-    if (idx === 0 || idx === normalizedTokens.length - 1) {
-      return titleCaseWord(normalizedWord);
-    }
-    return isSpecialWord(normalizedWord) ? normalizedWord : titleCaseWord(normalizedWord);
-  });
-  return untokenize(titleCasedTokens);
+  return untokenize(
+    tokenize(title)
+      .map(normalize)
+      .map((normalizedWord, idx, arr) => {
+        if (idx === 0 || idx === arr.length - 1) {
+          return titleCaseWord(normalizedWord);
+        }
+        return isSpecialWord(normalizedWord) ? normalizedWord : titleCaseWord(normalizedWord);
+      })
+  );
 }
 
 export default {
